518b3f7466d44b837f567a1154275e763dfbbb4d
Peer Richelsen <founders@mage.market>
2020-04-16 15:04:03 +0200

    minor changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,7 +20,7 @@ export default function MyApp(props: AppProps) {
   return (
   <React.Fragment>
     <Head>
-      <title>My page</title>
+      <title>Bookface Redesign</title>
       <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
     </Head>
     <ThemeProvider theme={theme}>
